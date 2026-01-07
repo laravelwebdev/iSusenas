25e8a5bc4a09ec2b81c8506537e1233d8c1fe012
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2026-01-08 07:54:53 +0800

    fix tarif listrik

--- listrik.js
+++ listrik.js
@@ -58,36 +58,36 @@ new Vue({
       if (this.jenis==="0") {
         switch(this.daya) {
           case "0":
-            return (0.91*this.jumlah/415*2).toFixed(1)
+            return (0.91*this.jumlah/415).toFixed(1)
           case "1":
-            return (0.91*this.jumlah/605*2).toFixed(1)
+            return (0.91*this.jumlah/605).toFixed(1)
           case "2":
-            return (0.91*this.jumlah/1352*2).toFixed(1)
+            return (0.91*this.jumlah/1352).toFixed(1)
           default:
-            return (0.91*this.jumlah/1444.7*2).toFixed(1)
+            return (0.91*this.jumlah/1444.7).toFixed(1)
         } 
       } else
       switch(this.daya) {
         case "0":
           if (100*this.jumlah/111-4950 < 5070) {
-            return ((100*this.jumlah/111-4950)/169*2).toFixed(1)
+            return ((100*this.jumlah/111-4950)/169).toFixed(1)
           }
           else if (100*this.jumlah/111-4950 < 15870) {
-            return (30+(100*this.jumlah/111-4950-5070)/360*2).toFixed(1)
+            return (30+(100*this.jumlah/111-4950-5070)/360).toFixed(1)
           }
-          else return (60+(100*this.jumlah/111-4950-15870)/495*2).toFixed(1)
+          else return (60+(100*this.jumlah/111-4950-15870)/495).toFixed(1)
         case "1":
           if (100*this.jumlah/111-18000 < 5500) {
-            return ((100*this.jumlah/111-18000)/275*2).toFixed(1)
+            return ((100*this.jumlah/111-18000)/275).toFixed(1)
           }
           else if (100*this.jumlah/111-18000 < 23300) {
-            return (20+(100*this.jumlah/111-18000-5500)/445*2).toFixed(1)
+            return (20+(100*this.jumlah/111-18000-5500)/445).toFixed(1)
           }
-          else return (60+(100*this.jumlah/111-18000-23300)/495*2).toFixed(1)
+          else return (60+(100*this.jumlah/111-18000-23300)/495).toFixed(1)
         case "2":
-          return (0.91*this.jumlah/1352*2).toFixed(1)
+          return (0.91*this.jumlah/1352).toFixed(1)
         default:
-          return (0.91*this.jumlah/1444.7*2).toFixed(1)
+          return (0.91*this.jumlah/1444.7).toFixed(1)
           
       } 
       }
