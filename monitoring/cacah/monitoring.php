3d7f3eea7e9080876b2b57f2e0c9452c09a635fe
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2026-01-08 09:43:02 +0800

    error

--- monitoring/cacah/monitoring.php
+++ monitoring/cacah/monitoring.php
@@ -3,6 +3,10 @@
  * Monitoring cacah data export
  */
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once '../../config/database.php';
 
 
