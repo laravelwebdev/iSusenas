4be8387d0622676f2e530c7314e80ed515fe7cb0
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2026-01-08 09:29:25 +0800

    fix export monitoring

--- monitoring/cacah/monitoring.php
+++ monitoring/cacah/monitoring.php
@@ -9,24 +9,20 @@ require_once '../../config/database.php';
     $conn = getDbConnection();
     $setRec = mysqli_query($conn, "SELECT * FROM cacah ORDER BY nks ASC, nus + 0 ASC");
     
-    $columnHeader = '';
     $columnHeader = "id" . "\t" . "prov" . "\t" . "kab" . "\t" . "nks" . "\t" . "nus" . "\t" . 
-                    "nus0324" . "\t" . "krt" . "\t" . "krt0324" . "\t" . "pcl" . "\t" . "pml" . "\t";
+                    "nus0324" . "\t" . "krt" . "\t" . "krt0324" . "\t" . "pcl" . "\t" . "pml" . "\t" .
  "pendidikan"."\t".
  "cacah"."\t".
  "p1c"."\t".
  "p2c"."\t".
- "periksa"."\t".
  "p1p"."\t".
  "p2p"."\t".
  "p3p"."\t".
  "p4p"."\t".
  "p5p"."\t".
- "kirim"."\t".
  "p1k"."\t".
  "p2k"."\t".
  "p3k"."\t".
- "terima"."\t".
  "p1t"."\t".
  "data"."\t";
 
